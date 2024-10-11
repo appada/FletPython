bb04398a12e6ae5ceb8324541e703999e7f94181
appada <129910854+appada@users.noreply.github.com>
2024-10-11 17:24:57 +0900

    Create 3-7-2_data_list.py

---
+++ basicFlet/3-7-2_data_list.py
@@ -0,0 +1,24 @@
+class Product:
+    def __init__(self, name, image_path, price):
+        self.name = name
+        self.image_path = image_path
+        self.price = price
+
+productList = [
+    Product('아아', '../images/아아.png', 3000),
+    Product('뜨아', '../images/뜨아.png', 4000),
+    Product('카페라떼', '../images/카페라떼.png', 5000),
+    Product('에스프레소아', '../images/에스프레소.png', 4000),
+    Product('카푸치노', '../images/카푸치노.png', 3000),
+    Product('초코라떼', '../images/초코라떼.png', 2000),
+    Product('흑당라떼', '../images/흑당라떼.png', 500),
+    Product('단팥라떼', '../images/단팥라떼.png', 3000),
+    Product('녹차라떼', '../images/녹차라떼.png', 3000),
+    Product('밀크쉐이크', '../images/밀크쉐이크.png', 4000),
+    Product('레몬', '../images/레몬.png', 5000),
+    Product('유자', '../images/유자.png', 4000),
+    Product('아이스티', '../images/아이스티.png', 4000),
+    Product('치즈케익', '../images/치즈케익.png', 8000),
+    Product('허니브레드', '../images/허니브레드.png', 8000),
+    Product('와플', '../images/와플.png', 8000),
+]
